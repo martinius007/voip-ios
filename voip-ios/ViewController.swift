8413d6842a695e43b703cf7f19bc97de58bb28cc
2FED <notfoolen@Timurs-MacBook-Pro.local>
2017-03-31 18:57:15 +0300

    init application

---
+++ voip-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  voip-ios
+//
+//  Created by Timur Nutfullin on 31/03/2017.
+//  Copyright © 2017 tim notfoolen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
